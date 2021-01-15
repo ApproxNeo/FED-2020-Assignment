5d244e4d9745bf4f8dff2958702281ec4e0f862c
Your Name <clementnjk@gmail.com>
2021-01-15 12:50:59 +0800

    a

--- js/index-slideshow/slideshow.js
+++ js/index-slideshow/slideshow.js
@@ -3,7 +3,7 @@
 function fadeImage(I) {
     var Image = '#' + I; //the id of each image
     $(Image).fadeIn();
-    $(Image).fadeOut(3000, function() { fadeImage((I == 1) ? 4 : I - 1); });
+    $(Image).fadeOut(3000, function () { fadeImage((I == 1) ? 4 : I - 1); });
 } 
 
 
