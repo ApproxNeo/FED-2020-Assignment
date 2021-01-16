c4c394cf3c03222cc313a83b21c82c4117f4028e
s10205832@connect.np.edu.sg <s10205832@connect.np.edu.sg>
2021-01-17 02:10:50 +0800

    editted

--- js/script.js
+++ js/script.js
@@ -1,11 +1,4 @@
 // JavaScript source code
-function myMap() {
-var mapProp= {
-  center:new google.maps.LatLng(51.508742,-0.120850),
-  zoom:5,
-};
-var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-}
 
 //Tell that you have submitted your work!
 $("form").submit(function (event) {
